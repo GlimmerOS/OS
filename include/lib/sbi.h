4995e7ffef12cd6e9a0063cbf0b3e479244f31e4
Liquidzk <492928615@qq.com>
2023-03-26 21:46:55 +0800

    bits_bug

--- include/lib/sbi.h
+++ include/lib/sbi.h
@@ -1,20 +1,7 @@
 #ifndef _SBI_H
 #define _SBI_H
 
-struct sbiret {
-  long error;
-  long value;
-};
 
-struct sbiret sbi_debug_console_read(unsigned long num_bytes,
-  unsigned long base_addr_lo,
-  unsigned long base_addr_hi);
-
-struct sbiret sbi_debug_console_write(unsigned long num_bytes,
-  unsigned long base_addr_lo,
-  unsigned long base_addr_hi);
-
-struct sbiret sbi_debug_console_write_byte(uint8_t byte);
 
 
 #endif
