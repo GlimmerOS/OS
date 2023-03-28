5173cc23611c1b18094a0f42dfcbacaf207a165d
22050908-Meng Pei <2398696309@qq.com>
2023-03-28 19:33:24 +0800

    Fix sib_debug_console_write bug

--- kernel/lib/sbi.c
+++ kernel/lib/sbi.c
@@ -3,5 +3,5 @@
 struct sbiret sbi_debug_console_write(unsigned long num_bytes,
                                       unsigned long base_addr_lo,
                                       unsigned long base_addr_hi) {
-  return sbi_call(DEBUG_CONSOLE_EXTENSION_ID, 0, 0, 0, 0, 0, 0);
+  return sbi_call(DEBUG_CONSOLE_EXTENSION_ID, 0, base_addr_lo, base_addr_hi, 0, 0, 0);
 }
