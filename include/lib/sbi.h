57116706a51dc9e075885ce37779eabaafc1032e
Liquidzk <492928615@qq.com>
2023-03-28 18:30:10 +0800

    no

--- include/lib/sbi.h
+++ include/lib/sbi.h
@@ -1,7 +1,22 @@
 #ifndef _SBI_H
 #define _SBI_H
+#include "lib/stdint.h"
 
 
+struct sbiret {
+  long error;
+  long value;
+};
+
+struct sbiret sbi_debug_console_read(unsigned long num_bytes,
+  unsigned long base_addr_lo,
+  unsigned long base_addr_hi);
+
+struct sbiret sbi_debug_console_write(unsigned long num_bytes,
+  unsigned long base_addr_lo,
+  unsigned long base_addr_hi);
+
+struct sbiret sbi_debug_console_write_byte(uint8_t byte);
 
 
 #endif
--- kernel/lib/sbi.c
+++ kernel/lib/sbi.c
@@ -1,2 +1,13 @@
 #include "lib/sbi.h"
-#include "stdint.h"
+
+struct sbiret sbi_debug_console_read(unsigned long num_bytes,
+  unsigned long base_addr_lo,
+  unsigned long base_addr_hi) {
+    
+}
+
+struct sbiret sbi_debug_console_write(unsigned long num_bytes,
+  unsigned long base_addr_lo,
+  unsigned long base_addr_hi);
+
+struct sbiret sbi_debug_console_write_byte(uint8_t byte);
