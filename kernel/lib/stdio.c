bca7ef466e161945d2763b518ab37b73c4e3a2d1
22050908-Meng Pei <2398696309@qq.com>
2023-03-28 23:11:41 +0800

    Finish printf

--- kernel/lib/stdio.c
+++ kernel/lib/stdio.c
@@ -7,10 +7,11 @@ int putchar(int ch) {
 }
 
 enum {
-  TY_INT32,
-  TY_INT64,
-  TY_UINT32,
-  TY_UINT64,
+  TY_INT32_10,
+  TY_INT64_10,
+  TY_UINT32_10,
+  TY_UINT64_10,
+  TY_STRING,
 };
 
 static void print_int32(int32_t num, int base);
@@ -23,11 +24,14 @@ static struct rule {
   char *target;
   int type;
 } rules[] = {
-  {"d", TY_INT32},
-  {"ld", TY_INT64},
-  ("u", TY_UINT32),
-  {"lu", TY_UINT64},
+  {"d", TY_INT32_10},
+  {"ld", TY_INT64_10},
+  {"u", TY_UINT32_10},
+  {"lu", TY_UINT64_10},
+  {"s", TY_STRING},
 };
+
+#define NR_RULES ARRLEN(rules)
 
 static void print_int32(int32_t num, int base) {
   char buf[32];
@@ -106,11 +110,52 @@ static void print_string(char *s) {
 void printf(char *fmt, ...) {
   va_list ap;
   size_t pos = 0;
+  size_t num;
+  char *s;
   
   va_start(ap, fmt);
   while (fmt[pos] != '\0') {
     if (fmt[pos] == '%') {
       pos++;
+
+      int i;
+      for (i = 0; i < NR_RULES; ++i) {
+        if (strncmp(fmt+pos, rules[i].target, strlen(rules[i].target)) == 0) {
+          break;
+        }
+      }
+
+      if (i == NR_RULES) {
+        // unknown format
+        putchar('%');
+        putchar(fmt[pos]);
+      } else {
+        // valid format
+        switch (rules[i].type) {
+          case TY_INT32_10:
+            num = va_arg(ap, int32_t);
+            print_int32(num, 10);
+            break;
+          case TY_INT64_10:
+            num = va_arg(ap, int64_t);
+            print_int64(num, 10);
+            break;
+          case TY_UINT32_10:
+            num = va_arg(ap, uint32_t);
+            print_uint32(num, 10);
+            break;
+          case TY_UINT64_10:
+            num = va_arg(ap, uint64_t);
+            print_uint64(num, 10);
+            break;
+          case TY_STRING:
+            s = va_arg(ap, char *);
+            print_string(s);
+            break;
+          default:
+            break;
+        }
+      }
     }
     pos++;
   }
