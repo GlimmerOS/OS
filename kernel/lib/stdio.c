2a633f6db11b5135432be3251433a65cf1228bf4
22050908-Meng Pei <2398696309@qq.com>
2023-04-21 17:50:23 +0800

    stdio framework

--- kernel/lib/stdio.c
+++ kernel/lib/stdio.c
@@ -3,7 +3,116 @@
 #include "string.h"
 #include "common.h"
 
-// static char digits[] = "0123456789abcdef";
+static char digits[] = "0123456789abcdef";
+static int out_target = 0; // 0 is stdout, 1 is buffer
+static int base = 0; // out integer base
+static int negtive = 0; // 0 is unsigned, 1 is signed
+
+static long number = 0; // the number need to print
+static char* string = 0; // the string need to print
+
+static char* buffer = 0; // out buffer
+
+static int ret = 0; // the return value of print function
+
+static void output(int ch) {
+  if (out_target == 0) {
+    sbi_console_putchar(ch);
+  } else {
+    *buffer = ch;
+    buffer++;
+  }
+}
+
+static void printf_num() {
+  ret++;
+  char buf[30];
+  int i = 0;
+
+  if (negtive == 1) {
+    if (number < 0) {
+      output('-');
+      number = -number;
+    }
+
+    while (number) {
+      buf[i++] = digits[number % base];
+      number /= base;
+    }
+  } else {
+    unsigned long unumber = number;
+    while (unumber) {
+      buf[i++] = digits[unumber % base];
+      unumber /= base;
+    }
+  }
+
+  while (i > 0) {
+    output(buf[--i]);
+  }
+}
+
+static void print_string() {
+  ret++;
+  while (*string) {
+    output(*string);
+    string++;
+  }
+}
+
+int printf(const char *format, ...) {
+  ret = 0;
+
+  va_list ap;
+  va_start(ap, format);
+  ret = vprintf(format, ap);
+  va_end(ap) ;
+  
+  return ret;
+}
+
+int vprintf(const char *format, va_list ap) {
+  return ret;
+}
+
+int vsprintf(char *out, const char *format, va_list ap) {
+  ret = 0;
+
+  out_target = 1;
+  buffer = out;
+
+  vprintf(format, ap);
+
+  *buffer = '\0';
+  buffer = 0;
+  out_target = 0;
+
+  return ret;
+}
+
+int sprintf(char *out, const char *format, ...) {
+  ret = 0;
+
+  va_list ap;
+  va_start(ap, format);
+  ret = vsprintf(out, format, ap);
+  va_end(ap) ;
+  
+  return ret;
+}
+
+int snprintf(char *out, size_t n, const char *format, ...) {
+  return 0;
+}
+
+int vsnprintf(char *out, size_t n, const char *format, va_list ap) {
+  return 0;
+}
+
+int putchar(int ch) {
+  output(ch);
+  return ch;
+}
 
 /**
  * 打印LOGO
