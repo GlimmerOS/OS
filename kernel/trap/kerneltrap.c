101bed3ebd115972b65af5b44213add36073ab3d
22050908-Meng Pei <2398696309@qq.com>
2023-04-25 14:29:01 +0800

    init_timerrupt

--- kernel/trap/kerneltrap.c
+++ kernel/trap/kerneltrap.c
@@ -1,6 +1,17 @@
 #include "common.h"
 #include "kernel.h"
 #include "debug.h"
+#include "lib/legacysbi.h"
+
+#define INTERRUPT_TIME 0x100000l
+
+static uint64_t read_time() {
+  return READ_CSR(, time);
+}
+
+static void set_next_timer() {
+  sbi_set_timer(read_time() + INTERRUPT_TIME);
+}
 
 void kernelvec();
 
@@ -8,9 +19,15 @@ void kernel_trap_init() {
   Log("Set stvec");
   WRITE_CSR(s, tvec, (uint64_t)kernelvec);
   Log("Finish set stvec");
+  
+  set_next_timer();
 }
 
 void kernel_trap_handler() {
-  Log("scause: %ld", READ_CSR(s, cause));
-  Log("sstatus: %ld", READ_CSR(s, status));
+  uint64_t scause = READ_CSR(s, cause);
+
+  if (scause == 0x8000000000000005) {
+    Log("Timer Interrupt!");
+    set_next_timer();
+  }
 }
