3a93cb59730949c4d45bab3c186c5fb0daa24eec
22050908-Meng Pei <2398696309@qq.com>
2023-04-25 20:47:00 +0800

    clang-format

--- kernel/lock/spinlock.c
+++ kernel/lock/spinlock.c
@@ -2,91 +2,90 @@
 #include "debug.h"
 
 /**
-*  初始化传入的锁
-*  @param lock 传入的锁
-*  @param name 锁的名字
-*/
-void init_lock(struct spinlock *lock, char *name)
-{
-    lock->cpu = 0;
-    lock->locked = 0;
-    lock->name = name;
+ *  初始化传入的锁
+ *  @param lock 传入的锁
+ *  @param name 锁的名字
+ */
+void init_lock(struct spinlock *lock, char *name) {
+  lock->cpu = 0;
+  lock->locked = 0;
+  lock->name = name;
 };
 
 /**
-*  获取传入的这个锁
-*  @param lock 传入的锁
-*/
+ *  获取传入的这个锁
+ *  @param lock 传入的锁
+ */
 void acquire(struct spinlock *lock) {
-    push_off();
+  push_off();
 
-    Assert(!holding(lock), "The lock is hold!");
-    
-    /*
-    * 这个__syn_lock_test_and_set 函数实际上是RISC-V 中的一次原子操作
-    * 具体是 a5 = arg2; s1 = arg1; amoswap.w.aq a5, a5, (s1)
-    * 而amoswap.w.aq rd, rs1, (rs1) 具体操作则是将rs1指向地址上的值填入rd，再将rs1的值填入rs1指向地址。此过程其他cpu无法访问rs2。
-    * 具体的思路：如果该锁被获得，locked置1,则执行该函数返回的也是1，继续循环
-    */
-    while (__sync_lock_test_and_set(&lock->locked, 1) != 0);
+  Assert(!holding(lock), "The lock is hold!");
 
-    //即fence 一次
-    __sync_synchronize();
-    
-    lock->cpu = mycpu();
+  /*
+   * 这个__syn_lock_test_and_set 函数实际上是RISC-V 中的一次原子操作
+   * 具体是 a5 = arg2; s1 = arg1; amoswap.w.aq a5, a5, (s1)
+   * 而amoswap.w.aq rd, rs1, (rs1)
+   * 具体操作则是将rs1指向地址上的值填入rd，再将rs1的值填入rs1指向地址。此过程其他cpu无法访问rs2。
+   * 具体的思路：如果该锁被获得，locked置1,则执行该函数返回的也是1，继续循环
+   */
+  while (__sync_lock_test_and_set(&lock->locked, 1) != 0)
+    ;
+
+  //即fence 一次
+  __sync_synchronize();
+
+  lock->cpu = mycpu();
 }
 
 /**
-*  释放传入的锁
-*  @param lock 传入的锁
-*/
+ *  释放传入的锁
+ *  @param lock 传入的锁
+ */
 void release(struct spinlock *lock) {
-    Assert(holding(lock), "The lock isn't hold");
+  Assert(holding(lock), "The lock isn't hold");
 
-    lock->cpu = 0;
+  lock->cpu = 0;
 
-    //fence 一次
-    __sync_synchronize();
+  // fence 一次
+  __sync_synchronize();
 
-    __sync_lock_release(&lock->locked);
+  __sync_lock_release(&lock->locked);
 
-    pop_off();
+  pop_off();
 }
 
 /**
-*  判断当前线程是否持有这个锁
-*  @param lock 传入的锁
-*  @return 返回结果，1持有0未持有
-*/
+ *  判断当前线程是否持有这个锁
+ *  @param lock 传入的锁
+ *  @return 返回结果，1持有0未持有
+ */
 int holding(struct spinlock *lock) {
-    int result;
-    result = (lock->locked && lock->cpu == mycpu());
-    return result;
+  int result;
+  result = (lock->locked && lock->cpu == mycpu());
+  return result;
 }
 
 /// 关闭中断并且标记初次操作前的开关中断情况，便于恢复；允许多次调用来嵌套
 void push_off(void) {
-    int old = intr_get(); //获取当前S-mode中断禁止位
-    intr_off(); //关中断
-    if (mycpu()->noff == 0)
-    {
-        mycpu()->intena = old; //标记在第一次push_off前是否为开启中断状态，便于之后恢复
-    }
-    mycpu()->noff += 1;
+  int old = intr_get(); //获取当前S-mode中断禁止位
+  intr_off();           //关中断
+  if (mycpu()->noff == 0) {
+    mycpu()->intena =
+        old; //标记在第一次push_off前是否为开启中断状态，便于之后恢复
+  }
+  mycpu()->noff += 1;
 }
 
 /// push_off的对称函数，将push_off的嵌套层数减一，到无嵌套时则恢复原状态
 void pop_off(void) {
-    struct cpu *cpu_ptr = mycpu();
+  struct cpu *cpu_ptr = mycpu();
 
-    Assert(!intr_get(), "Cannot open the interupt");
-    Assert(cpu_ptr->noff >= 1, "cpu_ptr->noff < 1");
+  Assert(!intr_get(), "Cannot open the interupt");
+  Assert(cpu_ptr->noff >= 1, "cpu_ptr->noff < 1");
 
-    cpu_ptr->noff -= 1;
-    if (cpu_ptr == 0 && cpu_ptr->intena)
-    {
-        intr_on();
-        /* 若是push_off引起的禁止中断，则将中断开启 */
-    }
-    
+  cpu_ptr->noff -= 1;
+  if (cpu_ptr == 0 && cpu_ptr->intena) {
+    intr_on();
+    /* 若是push_off引起的禁止中断，则将中断开启 */
+  }
 }
--- kernel/process/process.c
+++ kernel/process/process.c
@@ -1,129 +1,136 @@
 #include "debug.h"
 #include "kernel.h"
 #include "stdc.h"
+
 static uint32_t pid;
 struct PCB PCB;
+
 extern char trampoline[];
 extern void swtch(struct Trapframe *old, struct Trapframe *new);
-void processSetup(struct Process* process)
-{
-   acquire(&process->lock);
-    printf("Process init ......\n");
-    process->processID=pid++;
-    processPgtableAlloc(process);
-    if(!process->pagetable)
-    {
-        Assert(0,"processInitFail:pagetable disalloc");
-    }
-     if((process->trapframe = (struct Trapframe *)alloc_physic_page()) == 0){
-         Assert(0,"processInitFail:trapFrame disalloc");
-        }
-        memset(process->trapframe,0,sizeof(process->trapframe));
-        process->trapframe->ra=(uint64_t)processFirstRun;
-         process->trapframe->sp=process->stack_inKenl+PAGE_SIZE;
-          
-        process->chan=0;
-    process->state=READY;
-    release(&process->lock);
-}
-void processesInit()
-{
-    struct Process *process;
-    pid=0;
-    printf("Processes first init ......\n");
-    init_lock(&PCB.lock,"PCBs");
-    for(process=PCB.process;process<&PCB.process[PCB_NUM];process++)
-    {
-        init_lock(&process->lock,"process_each");
-        process->state=UNUSED;
-        process->stack_inKenl=Process_Stack(process-PCB.process);
-        //here Process_Stack is not allocated,need memory in kernel
-    }
-    mycpu()->trapframe->ra=(uint64_t)scheduler;
-    // mycpu()->trapframe.kernelSp=
-}
-struct Process* allocProcess()
-{
-    struct Process *p;
-    acquire(&PCB.lock);
-    for(p=PCB.process;p<&PCB.process[PCB_NUM];p++)
-    {   
-        acquire(&p->lock);
-        if(p->state==UNUSED)
-        {
-            processSetup(p);
-            release(&p->lock);
-            release(&PCB.lock);
-            return p;
-        }
-        release(&p->lock);
-    }
-    release(&PCB.lock);
-    return 0;
-}
-void processFirstRun()
-{
-    release(&myProcess()->lock);
-    //here a func to return from kernel to user space is needed.
-}
-struct Process*myProcess()
-{
-    return mycpu()->process;
-}
-void scheduler()
-{
-    struct Process *process;
-    struct cpu* cpu=mycpu();
-    while(1)
-    {
-        for(process=PCB.process;process<PCB.process;process++)
-        {
-            acquire(&process->lock);
-            if(process->state==UNUSED)
-            {
-                process->state=RUNNING;
-                cpu->process=process;
-                swtch(cpu->trapframe, process->trapframe);
-            }
-            release(&process->lock);
-        }
-    }
-}
-void switch2Scheduler()
-{
-    struct Process* process=myProcess();
-    int intena;
-    if(!holding(&process->lock))
-    {
-        Assert(0,"switch2Scheduler:proccess lock unheld");
-    }
-    if(process->state==RUNNING)
-    {
-        Assert(0,"myProcess is still running");
-    }
-    intena=mycpu()->intena;
-    swtch(process->trapframe,mycpu()->trapframe);
-    mycpu()->intena=intena;
-}
-void
-processPgtableAlloc(struct Process *process)
-{
-  // An empty page table.
-  process->pagetable = alloc_physic_page();
-  if(process->pagetable == 0)
-    Assert(0,"process pgtable Alloc fail");
 
-  if(va_map_pa(process->pagetable, TRAMPOLINE, (uint64_t)trampoline, MPTE_FLAG(R)|MPTE_FLAG(W)) < 0){
-    va_unmap_pa(process->pagetable,TRAMPOLINE);
-    free_physic_page(process->pagetable);
-    Assert(0,"TRAMPOLINE map failed");
+void processSetup(struct Process *process) {
+  Log("Process init...");
+
+  acquire(&process->lock);
+  process->processID = pid++;
+  processPgtableAlloc(process);
+
+  Assert(process->pagetable, "processInitFail:pagetable disalloc");
+
+  Assert((process->trapframe = (struct Trapframe *)alloc_physic_page()) == 0, "processInitFail:trapFrame disalloc");
+
+  memset(process->trapframe, 0, sizeof(struct Trapframe));
+  process->trapframe->ra = (uint64_t)processFirstRun;
+  process->trapframe->sp = process->stack_inKenl + PAGE_SIZE;
+
+  process->chan = 0;
+  process->state = READY;
+
+  release(&process->lock);
+}
+
+void processesInit() {
+  Log("Processes first init ...");
+
+  struct Process *process;
+  pid = 0;
+
+  init_lock(&PCB.lock, "PCBs");
+
+  for (process = PCB.process; process < &PCB.process[PCB_NUM]; process++) {
+    init_lock(&process->lock, "process_each");
+    process->state = UNUSED;
+    process->stack_inKenl = Process_Stack(process - PCB.process);
+    // here Process_Stack is not allocated,need memory in kernel
   }
 
-  if(va_map_pa(process->pagetable, TRAPFRAME, (uint64_t)(process->trapframe), MPTE_FLAG(R)|MPTE_FLAG(W)) < 0){
+  mycpu()->trapframe->ra = (uint64_t)scheduler;
+  // mycpu()->trapframe.kernelSp=
+}
+
+struct Process *allocProcess() {
+  struct Process *p;
+
+  acquire(&PCB.lock);
+  for (p = PCB.process; p < &PCB.process[PCB_NUM]; p++) {
+    acquire(&p->lock);
+
+    if (p->state == UNUSED) {
+      processSetup(p);
+      release(&p->lock);
+      release(&PCB.lock);
+      return p;
+    }
+
+    release(&p->lock);
+  }
+
+  release(&PCB.lock);
+  return 0;
+}
+
+void processFirstRun() {
+  release(&myProcess()->lock);
+  // here a func to return from kernel to user space is needed.
+}
+
+struct Process *myProcess() {
+  return mycpu()->process;
+}
+
+void scheduler() {
+  struct Process *process;
+  struct cpu *cpu = mycpu();
+
+  while (1) {
+    for (process = PCB.process; process < PCB.process; process++) {
+      acquire(&process->lock);
+      if (process->state == UNUSED) {
+        process->state = RUNNING;
+        cpu->process = process;
+        swtch(cpu->trapframe, process->trapframe);
+      }
+      release(&process->lock);
+    }
+  }
+}
+
+void switch2Scheduler() {
+  struct Process *process = myProcess();
+  int intena;
+
+  if (!holding(&process->lock)) {
+    Assert(0, "switch2Scheduler:proccess lock unheld");
+  }
+
+  if (process->state == RUNNING) {
+    Assert(0, "myProcess is still running");
+  }
+
+  intena = mycpu()->intena;
+  swtch(process->trapframe, mycpu()->trapframe);
+  mycpu()->intena = intena;
+}
+
+void processPgtableAlloc(struct Process *process) {
+  // An empty page table.
+  process->pagetable = alloc_physic_page();
+  if (process->pagetable == 0)
+    Assert(0, "process pgtable Alloc fail");
+
+  if (va_map_pa(process->pagetable, TRAMPOLINE, (uint64_t)trampoline,
+                MPTE_FLAG(R) | MPTE_FLAG(W)) < 0) {
     va_unmap_pa(process->pagetable, TRAMPOLINE);
-     va_unmap_pa(process->pagetable,TRAPFRAME);
     free_physic_page(process->pagetable);
-    Assert(0,"TRAPFRAME map failed");
+    Assert(0, "TRAMPOLINE map failed");
+  }
+
+  if (va_map_pa(process->pagetable, TRAPFRAME, (uint64_t)(process->trapframe),
+                MPTE_FLAG(R) | MPTE_FLAG(W)) < 0) {
+    va_unmap_pa(process->pagetable, TRAMPOLINE);
+    va_unmap_pa(process->pagetable, TRAPFRAME);
+    free_physic_page(process->pagetable);
+    Assert(0, "TRAPFRAME map failed");
   }
 }
 // static void freeproc(Process *process)
@@ -140,20 +147,18 @@ processPgtableAlloc(struct Process *process)
 //     process->state=UNUSED;
 
 // }
-void
-yield(void)
-{
+
+void yield(void) {
   struct Process *process = myProcess();
   acquire(&process->lock);
   process->state = READY;
   switch2Scheduler();
   release(&process->lock);
 }
-void
-sleep(void *chan, struct spinlock *lk)
-{
+
+void sleep(void *chan, struct spinlock *lk) {
   struct Process *process = myProcess();
-  
+
   // Must acquire p->lock in order to
   // change p->state and then call switch2Scheduler.
   // Once we hold p->lock, we can be
@@ -161,11 +166,11 @@ sleep(void *chan, struct spinlock *lk)
   // (wakeup locks p->lock),
   // so it's okay to release lk.
 
-  acquire(&process->lock);  //DOC: sleeplock1
+  acquire(&process->lock); // DOC: sleeplock1
   release(lk);
 
   // Go to sleep.
-  process->chan=chan;
+  process->chan = chan;
   process->state = SLEEPING;
 
   switch2Scheduler();
@@ -178,36 +183,42 @@ sleep(void *chan, struct spinlock *lk)
   acquire(lk);
 }
 
-void wakeup(void *chan)
-{
-    struct Process *process;
-     for(process = PCB.process; process < &PCB.process[PCB_NUM]; process++) {
-    if(process != myProcess()){
+void wakeup(void *chan) {
+  struct Process *process;
+
+  for (process = PCB.process; process < &PCB.process[PCB_NUM]; process++) {
+    if (process != myProcess()) {
       acquire(&process->lock);
-      if(process->state == SLEEPING && process->chan == chan) {
+
+      if (process->state == SLEEPING && process->chan == chan) {
         process->state = READY;
       }
+
       release(&process->lock);
     }
   }
 }
 
-int kill(int pid){
-   struct Process *process;
+int kill(int pid) {
+  struct Process *process;
 
-  for(process = PCB.process; process < &PCB.process[PCB_NUM]; process++){
+  for (process = PCB.process; process < &PCB.process[PCB_NUM]; process++) {
     acquire(&process->lock);
-    if(process->processID == pid){
+
+    if (process->processID == pid) {
       process->killed = 1;
-      if(process->state == SLEEPING){
+
+      if (process->state == SLEEPING) {
         // Wake process from sleep().
         process->state = READY;
       }
+
       release(&process->lock);
       return 0;
     }
+
     release(&process->lock);
   }
+
   return -1;
 }
-
